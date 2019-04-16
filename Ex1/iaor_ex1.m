99dfcc19920d7f7dea322fec836680e9cb5bf934
Hans <hans.lienhop@gmail.com>
2019-04-16 15:38:47 +0200

    added contrast stretching

--- Ex1/iaor_ex1.m
+++ Ex1/iaor_ex1.m
@@ -1,3 +1,30 @@
-J = imread("input_sat_image.jpg");
-imshow(J);
-imhist(J)
+function iaor_ex1
+  J = imread("input_sat_image.jpg");
+  #figure, imshow(J);
+  #figure, imhist(J);
+  #stretched = imadjust(J, stretchlim(J, [0, 1]));
+  #figure, imshow(stretched);
+  #figure, imhist(stretched);
+  imgStretched = stretched(J);
+  figure, imshow(imgStretched);
+  figure, imhist(imgStretched); 
+end
+
+function stretched = stretched(img) 
+  imgTemp = img(:,:,1);
+  minTemp = min(imgTemp);         
+  minFinal = min(minTemp);
+  maxTemp = max(imgTemp);       
+  maxFinal = max(maxTemp);
+
+  middle = maxFinal - minFinal;
+  disp(minFinal)
+  disp(maxFinal)
+
+  for i = 1:size(imgTemp,1)
+      for j = 1:size(imgTemp,2)
+       imgTemp(i, j) = (double((imgTemp(i,j) - minFinal))/double(middle))*250;
+      endfor
+  endfor      
+  stretched = imgTemp;
+end
