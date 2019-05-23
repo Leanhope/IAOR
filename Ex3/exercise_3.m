71216e7f6cce0f06ea70ebd4099c294e6ed8055e
Leanhope <hans.lienhop@gmail.com>
2019-05-23 16:30:18 +0200

    Exercise 3, most done

--- Ex3/exercise_3.m
+++ Ex3/exercise_3.m
@@ -1,24 +1,36 @@
 function exercise_3
-  im = imread("input_ex3.jpg");
-  [imx, imy] = gaussgradient(im, 5);
-  imshow(imx)
+    im = rgb2gray(imread("input_ex3.jpg"));
+    imStretch = imadjust(im,stretchlim(im),[]);
+    [imx, imy] = gaussgradient(imStretch, 0.5);
+    magnitude = abs(imx)+abs(imy);
+    edges = edge(magnitude,'Canny', 0.1);
+    [H,R,T] = houghVoting(edges);
+    
+    peaks = 10;
+    P = houghpeaks(H, peaks);
+    imshow(H,[]);
+    hold on;
+    plot(P(:, 2), P(:, 1),'s','color','red')
 end
 
-function [Gx,Gy] = gaussgradient(img, sigma)
-    radius = 3 * sigma; % filter kernel radius
-    size = 5;
-    Gx = zeros(5,5);
-
-    for i=1:size
-        for j=1:size
-            u=[i-radius-1 j-radius-1];
-            Gx(i,j)= (exp(-u(1)^2/(2*sigma^2)) / (sigma*sqrt(2*pi))) * ...
-            (-u(2) * exp(-u(2)^2/(2*sigma^2)) / (sigma*sqrt(2*pi)) / sigma^2);
+function[H, Rho, Theta] = houghVoting(binMask)
+    [n_row, n_col] = size(binMask);
+    rho_max = floor(sqrt(n_row^2+n_col^2));
+    
+    H = (zeros(2*rho_max+1, 180));    
+    Rho = [-rho_max, rho_max -1];
+    Theta = [-90, 89];
+    
+    for row = 1:n_row
+        for col = 1:n_col
+            if binMask(row, col) == 1
+                x = col - 1;
+                y = row - 1;
+                for theta = -90:89
+                    rho = round(x * cosd(theta) + y * sind(theta));
+                    H(rho + rho_max + 1, theta + 91) = H(rho + rho_max + 1, theta + 91) + 1;
+                end
+            end
         end
     end
-
-    Gy=Gx';
-
-    Gx=imfilter(img,Gx,'conv');
-    Gy=imfilter(img,Gy,'conv');
 end
