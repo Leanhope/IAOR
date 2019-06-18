a66ebbc438e3a15f2a78c205fbe52d789a34b0ed
fulyata <fulya.tasliarmut@uni-weimar.de>
2019-06-19 00:31:07 +0200

    fix input and plotting

--- Ex4/exercise_4.m
+++ Ex4/exercise_4.m
@@ -2,7 +2,7 @@
 % Pia Fichtl (114545), Hans Lienhop (114926), Fulya Tasliarmut (111448)
 % ===============================
 % Task A - Image filtering
-%pkg load image;
+pkg load image;
 % Task a
 imgA = im2double(rgb2gray(imread('taskA.png')));
 % Task b
@@ -31,10 +31,10 @@ g_pad_fft = fft2(g_pad);
 fil_fft = g_pad_fft .* imn_fft;
 % apply inverse FFT
 fil = ifft2(fil_fft);
-%figure; imagesc(log(abs(fftshift(imn_fft)))); title('Logarithmic centered image spectra');
-%figure; imagesc(log(abs(fftshift(g_pad_fft)))); title('Logarithmic circular shifted padded Gaussian filter spectra');
-%figure; imagesc(log(abs(fftshift(fil_fft)))); title('Logarithmic centered product of spatial and frequency domain')
-%figure; imshow(fil); title('Filtered image after inverse FFT');
+figure; imagesc(log(abs(fftshift(imn_fft)))); title('Logarithmic centered image spectra');
+figure; imagesc(log(abs(fftshift(g_pad_fft)))); title('Logarithmic circular shifted padded Gaussian filter spectra');
+figure; imagesc(log(abs(fftshift(fil_fft)))); title('Logarithmic centered product of spatial and frequency domain')
+figure; imshow(fil); title('Filtered image after inverse FFT');
 
 % Task B - Shape recognition
 % Task a
@@ -42,45 +42,59 @@ trainB = im2double(rgb2gray(imread('trainB.png')));
 % Task b
 bmtrainB = im2bw(trainB,graythresh(trainB));
 % Task c
-[values_train, Df_train] = makeFourierDescriptor(bmtrainB);
-%figure; plot(Df_train); %Graph Fourier
+%[values_train, Df_train] = makeFourierDescriptor(bmtrainB);
+% create train fourier descriptor
+cells = bwboundaries(bmtrainB);
+b = cells{1};
+Df_train = complex(b(:,2), b(:,1));
+Df_train = fft(Df_train);
+n = 24;
+Df_train = Df_train(1:(n+1));
+% normalize
+Df_train = Df_train(2:(n+1));
+Df_train = Df_train / abs(Df_train(1));
+Df_train = abs(Df_train);
 
 
-test1B = im2double(rgb2gray(imread('test1B.jpg')));
-bmtest1B = imbinarize(test1B, graythresh(test1B));
+test1B = mat2gray(mean(imread('test1B.jpg'),3));
+bmtest1B = im2bw(test1B, graythresh(test1B));
 [valuesB1, img1descriptors] = makeFourierDescriptor(bmtest1B);
-figure; imshow(bmtest1B);
+figure; imshow(test1B); %imshow(bmtest1B);
 hold on
 for k = 1:length(img1descriptors)
-    norm(Df_train - img1descriptors(k, :))
-    if norm(Df_train - img1descriptors(k, :)) < 0.06
+      if norm(Df_train - img1descriptors(k,:)') < 0.06
         boundary = valuesB1{k};
         plot(boundary(:,2),  boundary(:,1), 'r', 'LineWidth', 2); 
-    end
-end
+      endif
+endfor
+hold off;
 
-test2B = im2double(rgb2gray(imread('test2B.jpg')));
-bmtest2B = imbinarize(test2B, graythresh(test2B));
+test2B = mat2gray(mean(imread('test2B.jpg'),3));
+bmtest2B = im2bw(test2B, graythresh(test2B));
 [valuesB2, img2descriptors] = makeFourierDescriptor(bmtest2B);
-figure; imshow(bmtest2B);
+figure; imshow(test2B); %imshow(bmtest2B);
 hold on
 for k = 1:length(img2descriptors)
-    norm(Df_train - img2descriptors(k, :))
-    if norm(Df_train - img2descriptors(k, :)) < 0.06
+    if norm(Df_train - img2descriptors(k, :)') < 0.06 % we could not find the ampelmaennchen with 0.06 threshold
         boundary = valuesB2{k};
         plot(boundary(:,2),  boundary(:,1), 'r', 'LineWidth', 2); 
-    end
-end
+    endif
+endfor
+hold off;
 
-test3B = im2double(rgb2gray(imread('test3B.jpg')));
-bmtest3B = imbinarize(test3B, graythresh(test3B));
+
+test3B = mat2gray(mean(imread('test3B.jpg'),3));
+bmtest3B = im2bw(test3B, graythresh(test3B));
 [valuesB3, img3descriptors] = makeFourierDescriptor(bmtest3B);
-figure; imshow(bmtest3B);
+figure; imshow(test3B)%imshow(bmtest3B);
 hold on
 for k = 1:length(img3descriptors)
-    norm(Df_train - img3descriptors(k, :))
-    if norm(Df_train - img3descriptors(k, :)) < 0.06
+    if norm(Df_train - img3descriptors(k, :)') < 0.07
+        y = k
+        x = norm(Df_train - img3descriptors(k, :)')
+        z = 2000
         boundary = valuesB3{k};
         plot(boundary(:,2),  boundary(:,1), 'r', 'LineWidth', 2); 
-    end
-end
+    endif
+endfor
+hold off
