960c3984a878f9657472c103a58d93dbc3f0bfdd
Leanhope <hans.lienhop@gmail.com>
2019-07-09 12:02:26 +0200

    Task 1 done

--- Ex5/exercise_5.m
+++ Ex5/exercise_5.m
@@ -2,17 +2,30 @@
 % Pia Fichtl (114545), Hans Lienhop (114926), Fulya Tasliarmut (111448)
 % ===============================
 
+%When using the image coordinates as the 4th and 5th dimension in the
+%k-Means algorithm, more centers and iterations were necessary to achieve
+%comparable results
+
 %img = imread('inputEx5_1.jpg');
 img = imread('inputEx5_2.jpg');
 
-k = 5; %amount of centers
+k = 3; %amount of centers
 d = 3; %number of dimensions
-n = 5; %number of iterations, when while loop takes too long
+n = 1; %number of iterations, when while takes too long...
 
 rgb = kMeans(img, k, d, n);
+figure;
+scatter3(rgb(:,1), rgb(:,2), rgb(:,3), [], rgb(:,d+1))
+b2img = reshape(rgb(:,d+1), [size(img, 1), size(img, 2)]);
+figure;
+imshow(imadjust(b2img,stretchlim(b2img),[]), 'Colormap',jet(255))
 
+k = 4;
+d = 5; %number of dimensions
+n = 2;
+rgb = kMeans(img, k, d, n);
 figure;
-scatter3(rgb(:,1), rgb(:,2), rgb(:,3), [], rgb(:,4))
-b2img = reshape(rgb(:,4), [size(img, 1), size(img, 2)]);
+scatter3(rgb(:,1), rgb(:,2), rgb(:,3), [], rgb(:,d+1))
+b2img = reshape(rgb(:,d+1), [size(img, 1), size(img, 2)]);
 figure;
-imshow(imadjust(b2img,stretchlim(b2img),[]))
+imshow(imadjust(b2img,stretchlim(b2img),[]), 'Colormap',jet(255))
